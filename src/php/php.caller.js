12c54f0e01fa1bb651de74ea12d7fc636fbe6394
Thiago Mata <thiago.henrique.mata@gmail.com>
2011-08-09 15:42:09 +0000

    php.js autoload caller

--- src/php/php.caller.js
+++ src/php/php.caller.js
@@ -3,37 +3,218 @@ php._phpjs_shared_bc = function () {
     return php.call_user_func_array( php._phpjs_shared_bc , arguments );
 }
 
-php.abs = function()  {
-    php.require_once( php.path + 'php.abs.js' );
+php.abs = function (arg) { 
+    php.require_once( php.path + 'php.abs.js' ); 
     return php.call_user_func_array( php.abs , arguments );
-}
-
-php.acos = function (arg) {
-    php.require_once( php.path + 'php.acos.js' );
+} 
+php.acos = function (arg) { 
+    php.require_once( php.path + 'php.acos.js' ); 
     return php.call_user_func_array( php.acos , arguments );
-}
-
-php.acosh = function (arg) {
-    php.require_once( php.path + 'php.acosh.js' );
+} 
+php.acosh = function (arg) { 
+    php.require_once( php.path + 'php.acosh.js' ); 
     return php.call_user_func_array( php.acosh , arguments );
-}
-
+} 
 php.addcslashes = function (arg) { 
     php.require_once( php.path + 'php.addcslashes.js' ); 
     return php.call_user_func_array( php.addcslashes , arguments );
-}
-
+} 
 php.addslashes = function (arg) { 
     php.require_once( php.path + 'php.addslashes.js' ); 
     return php.call_user_func_array( php.addslashes , arguments );
 } 
-
 php.aggregate = function (arg) { 
     php.require_once( php.path + 'php.aggregate.js' ); 
     return php.call_user_func_array( php.aggregate , arguments );
-}
-
+} 
 php.aggregate_info = function (arg) { 
     php.require_once( php.path + 'php.aggregate_info.js' ); 
     return php.call_user_func_array( php.aggregate_info , arguments );
 } 
+php.aggregate_methods = function (arg) { 
+    php.require_once( php.path + 'php.aggregate_methods.js' ); 
+    return php.call_user_func_array( php.aggregate_methods , arguments );
+} 
+php.aggregate_methods_by_list = function (arg) { 
+    php.require_once( php.path + 'php.aggregate_methods_by_list.js' ); 
+    return php.call_user_func_array( php.aggregate_methods_by_list , arguments );
+} 
+php.aggregate_methods_by_regexp = function (arg) { 
+    php.require_once( php.path + 'php.aggregate_methods_by_regexp.js' ); 
+    return php.call_user_func_array( php.aggregate_methods_by_regexp , arguments );
+} 
+php.aggregate_properties = function (arg) { 
+    php.require_once( php.path + 'php.aggregate_properties.js' ); 
+    return php.call_user_func_array( php.aggregate_properties , arguments );
+} 
+php.aggregate_properties_by_list = function (arg) { 
+    php.require_once( php.path + 'php.aggregate_properties_by_list.js' ); 
+    return php.call_user_func_array( php.aggregate_properties_by_list , arguments );
+} 
+php.aggregate_properties_by_regexp = function (arg) { 
+    php.require_once( php.path + 'php.aggregate_properties_by_regexp.js' ); 
+    return php.call_user_func_array( php.aggregate_properties_by_regexp , arguments );
+} 
+php.aggregation_info = function (arg) { 
+    php.require_once( php.path + 'php.aggregation_info.js' ); 
+    return php.call_user_func_array( php.aggregation_info , arguments );
+} 
+php.array = function (arg) { 
+    php.require_once( php.path + 'php.array.js' ); 
+    return php.call_user_func_array( php.array , arguments );
+} 
+php.array_change_key_case = function (arg) { 
+    php.require_once( php.path + 'php.array_change_key_case.js' ); 
+    return php.call_user_func_array( php.array_change_key_case , arguments );
+} 
+php.array_chunk = function (arg) { 
+    php.require_once( php.path + 'php.array_chunk.js' ); 
+    return php.call_user_func_array( php.array_chunk , arguments );
+} 
+php.array_combine = function (arg) { 
+    php.require_once( php.path + 'php.array_combine.js' ); 
+    return php.call_user_func_array( php.array_combine , arguments );
+} 
+php.array_count_values = function (arg) { 
+    php.require_once( php.path + 'php.array_count_values.js' ); 
+    return php.call_user_func_array( php.array_count_values , arguments );
+} 
+php.array_diff = function (arg) { 
+    php.require_once( php.path + 'php.array_diff.js' ); 
+    return php.call_user_func_array( php.array_diff , arguments );
+} 
+php.array_diff_assoc = function (arg) { 
+    php.require_once( php.path + 'php.array_diff_assoc.js' ); 
+    return php.call_user_func_array( php.array_diff_assoc , arguments );
+} 
+php.array_diff_key = function (arg) { 
+    php.require_once( php.path + 'php.array_diff_key.js' ); 
+    return php.call_user_func_array( php.array_diff_key , arguments );
+} 
+php.array_diff_uassoc = function (arg) { 
+    php.require_once( php.path + 'php.array_diff_uassoc.js' ); 
+    return php.call_user_func_array( php.array_diff_uassoc , arguments );
+} 
+php.array_diff_ukey = function (arg) { 
+    php.require_once( php.path + 'php.array_diff_ukey.js' ); 
+    return php.call_user_func_array( php.array_diff_ukey , arguments );
+} 
+php.array_fill = function (arg) { 
+    php.require_once( php.path + 'php.array_fill.js' ); 
+    return php.call_user_func_array( php.array_fill , arguments );
+} 
+php.array_fill_keys = function (arg) { 
+    php.require_once( php.path + 'php.array_fill_keys.js' ); 
+    return php.call_user_func_array( php.array_fill_keys , arguments );
+} 
+php.array_filter = function (arg) { 
+    php.require_once( php.path + 'php.array_filter.js' ); 
+    return php.call_user_func_array( php.array_filter , arguments );
+} 
+php.array_flip = function (arg) { 
+    php.require_once( php.path + 'php.array_flip.js' ); 
+    return php.call_user_func_array( php.array_flip , arguments );
+} 
+php.array_intersect = function (arg) { 
+    php.require_once( php.path + 'php.array_intersect.js' ); 
+    return php.call_user_func_array( php.array_intersect , arguments );
+} 
+php.array_intersect_assoc = function (arg) { 
+    php.require_once( php.path + 'php.array_intersect_assoc.js' ); 
+    return php.call_user_func_array( php.array_intersect_assoc , arguments );
+} 
+php.array_intersect_key = function (arg) { 
+    php.require_once( php.path + 'php.array_intersect_key.js' ); 
+    return php.call_user_func_array( php.array_intersect_key , arguments );
+} 
+php.array_intersect_uassoc = function (arg) { 
+    php.require_once( php.path + 'php.array_intersect_uassoc.js' ); 
+    return php.call_user_func_array( php.array_intersect_uassoc , arguments );
+} 
+php.array_intersect_ukey = function (arg) { 
+    php.require_once( php.path + 'php.array_intersect_ukey.js' ); 
+    return php.call_user_func_array( php.array_intersect_ukey , arguments );
+} 
+php.array_key_exists = function (arg) { 
+    php.require_once( php.path + 'php.array_key_exists.js' ); 
+    return php.call_user_func_array( php.array_key_exists , arguments );
+} 
+php.array_keys = function (arg) { 
+    php.require_once( php.path + 'php.array_keys.js' ); 
+    return php.call_user_func_array( php.array_keys , arguments );
+} 
+php.array_map = function (arg) { 
+    php.require_once( php.path + 'php.array_map.js' ); 
+    return php.call_user_func_array( php.array_map , arguments );
+} 
+php.array_merge = function (arg) { 
+    php.require_once( php.path + 'php.array_merge.js' ); 
+    return php.call_user_func_array( php.array_merge , arguments );
+} 
+php.array_merge_recursive = function (arg) { 
+    php.require_once( php.path + 'php.array_merge_recursive.js' ); 
+    return php.call_user_func_array( php.array_merge_recursive , arguments );
+} 
+php.array_multisort = function (arg) { 
+    php.require_once( php.path + 'php.array_multisort.js' ); 
+    return php.call_user_func_array( php.array_multisort , arguments );
+} 
+php.array_pad = function (arg) { 
+    php.require_once( php.path + 'php.array_pad.js' ); 
+    return php.call_user_func_array( php.array_pad , arguments );
+} 
+php.array_pop = function (arg) { 
+    php.require_once( php.path + 'php.array_pop.js' ); 
+    return php.call_user_func_array( php.array_pop , arguments );
+} 
+php.array_product = function (arg) { 
+    php.require_once( php.path + 'php.array_product.js' ); 
+    return php.call_user_func_array( php.array_product , arguments );
+} 
+php.array_push = function (arg) { 
+    php.require_once( php.path + 'php.array_push.js' ); 
+    return php.call_user_func_array( php.array_push , arguments );
+} 
+php.array_rand = function (arg) { 
+    php.require_once( php.path + 'php.array_rand.js' ); 
+    return php.call_user_func_array( php.array_rand , arguments );
+} 
+php.array_reduce = function (arg) { 
+    php.require_once( php.path + 'php.array_reduce.js' ); 
+    return php.call_user_func_array( php.array_reduce , arguments );
+} 
+php.array_replace = function (arg) { 
+    php.require_once( php.path + 'php.array_replace.js' ); 
+    return php.call_user_func_array( php.array_replace , arguments );
+} 
+php.array_replace_recursive = function (arg) { 
+    php.require_once( php.path + 'php.array_replace_recursive.js' ); 
+    return php.call_user_func_array( php.array_replace_recursive , arguments );
+} 
+php.array_reverse = function (arg) { 
+    php.require_once( php.path + 'php.array_reverse.js' ); 
+    return php.call_user_func_array( php.array_reverse , arguments );
+} 
+php.array_search = function (arg) { 
+    php.require_once( php.path + 'php.array_search.js' ); 
+    return php.call_user_func_array( php.array_search , arguments );
+} 
+php.array_shift = function (arg) { 
+    php.require_once( php.path + 'php.array_shift.js' ); 
+    return php.call_user_func_array( php.array_shift , arguments );
+} 
+php.array_slice = function (arg) { 
+    php.require_once( php.path + 'php.array_slice.js' ); 
+    return php.call_user_func_array( php.array_slice , arguments );
+} 
+php.array_splice = function (arg) { 
+    php.require_once( php.path + 'php.array_splice.js' ); 
+    return php.call_user_func_array( php.array_splice , arguments );
+} 
+php.array_sum = function (arg) { 
+    php.require_once( php.path + 'php.array_sum.js' ); 
+    return php.call_user_func_array( php.array_sum , arguments );
+} 
+php.array_udiff = function (arg) { 
+    php.require_once( php.path + 'php.array_udiff.js' ); 
+    return
