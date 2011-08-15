dd52661f07051c4fe9154e3884ecbed2a054f954
Thiago Mata <thiago.henrique.mata@gmail.com>
2011-08-15 18:42:13 +0000

    class already loaded

--- src/main/New.js
+++ src/main/New.js
@@ -1,49 +1,50 @@
-var New;
-New = (function() {
-  function New() {}
-  return New;
-})();
-New.prototype.arrMap = {
-  CanvasBox: './CanvasBox.coffee',
-  CanvasBoxButton: './CanvasBoxButton.coffee',
-  CanvasBoxConnector: './CanvasBoxConnector.coffee',
-  People: './People.coffee'
-};
-New.prototype.arrClasses = Array();
-New.prototype.loadClass = function(strClass) {
-  var strContent;
-  if (php.in_array(strClass, New.prototype.arrClasses)) {
-    alert("" + strCLass + " ja carregada");
-    return false;
-  }
-  if (!(New.prototype.arrMap[strClass] != null)) {
-    alert("" + strClass + " {nao existe");
-    throw new CanvasBoxException("Unabled to map the class " + strClass);
-  }
-  strContent = php.file_get_contents(New.prototype.arrMap[strClass]);
-  CoffeeScript.run(strContent);
-  return true;
-};
-New.prototype.construct = function(klass, args) {
-  var ObjectPointer;
-  ObjectPointer = function() {
-    return klass.apply(this, arguments[0]);
+ var New;
+  New = (function() {
+    function New() {}
+    return New;
+  })();
+  New.prototype.arrMap = {
+    CanvasBox: './CanvasBox.coffee',
+    CanvasBoxButton: './CanvasBoxButton.coffee',
+    CanvasBoxConnector: './CanvasBoxConnector.coffee',
+    People: './People.coffee'
   };
-  ObjectPointer.prototype = klass.prototype;
-  return new ObjectPointer(args);
-};
-New.prototype.start = function() {
-  var element, path, _ref, _results;
-  _ref = New.prototype.arrMap;
-  _results = [];
-  for (element in _ref) {
-    path = _ref[element];
-    _results.push(New[element] = new Function("return New.prototype.Instance({ name: '" + element + "', data: arguments });"));
-  }
-  return _results;
-};
-New.prototype.Instance = function(arrDataLoad) {
-  New.prototype.loadClass(arrDataLoad.name);
-  return New.prototype.construct(window[arrDataLoad.name], arrDataLoad.data);
-};
-New.prototype.start();
+  New.prototype.arrClasses = Array();
+  New.prototype.loadClass = function(strClass) {
+    var strContent;
+    if (php.in_array(strClass, New.prototype.arrClasses)) {
+      console.log("" + strClass + " already load");
+      return false;
+    }
+    if (!(New.prototype.arrMap[strClass] != null)) {
+      alert("" + strClass + " does not exists");
+      throw new CanvasBoxException("Unabled to map the class " + strClass);
+    }
+    strContent = php.file_get_contents(New.prototype.arrMap[strClass]);
+    New.prototype.arrClasses.push(strClass);
+    CoffeeScript.run(strContent);
+    return true;
+  };
+  New.prototype.construct = function(klass, args) {
+    var ObjectPointer;
+    ObjectPointer = function() {
+      return klass.apply(this, arguments[0]);
+    };
+    ObjectPointer.prototype = klass.prototype;
+    return new ObjectPointer(args);
+  };
+  New.prototype.start = function() {
+    var element, path, _ref, _results;
+    _ref = New.prototype.arrMap;
+    _results = [];
+    for (element in _ref) {
+      path = _ref[element];
+      _results.push(New[element] = new Function("return New.prototype.Instance({ name: '" + element + "', data: arguments });"));
+    }
+    return _results;
+  };
+  New.prototype.Instance = function(arrDataLoad) {
+    New.prototype.loadClass(arrDataLoad.name);
+    return New.prototype.construct(window[arrDataLoad.name], arrDataLoad.data);
+  };
+  New.prototype.start();
